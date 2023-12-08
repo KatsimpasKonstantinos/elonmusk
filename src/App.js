85d408b990ae2f8aaf6bfa53e585195beb539f7e
KatsimpasKonstantinos <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2023-12-08 12:07:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  let [counter, updateCounter] = useState(0);
   return (
     <div className="App">
       <header className="App-header">
         <h1>
           No.
         </h1>
-        <p> People waiting: {getCounter()} </p>
+        <p> People waiting: {counter} </p>
+        <button onClick="putCounter()"> Wait </button>
       </header>
     </div>
   );
@@ -20,13 +23,14 @@ async function getCounter() {
   const response = await fetch(workerURL, {
     method: 'POST',
   });
-  return response.data;
+  updateCounter(response.data);
 }
 
-async function updateCounter() {
+async function putCounter() {
   const response = await fetch(workerURL, {
     method: 'PUT',
   });
+  updateCounter(response.data);
 }
 
 export default App;
