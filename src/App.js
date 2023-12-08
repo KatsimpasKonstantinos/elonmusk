432d48de72f151e4dc535b6ee5702ad32cac8370
KatsimpasKonstantinos <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2023-12-08 13:09:07 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,49 +1,59 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
-import { useState, useEffect } from "react";
 
 function App() {
-  let [counter, updateCounter] = useState(0);
+  const [counter, setCounter] = useState(0);
+  const [buttonDisabled, setButtonDisabled] = useState(false);
 
   const workerURL = "https://elonmuskdeathwaiters.konsti032003.workers.dev/";
 
-async function getCounter() {
-  const response = await fetch(workerURL, {
-    method: 'GET',
-  });
-  console.log(response);
-  updateCounter(await consumeStream(response));
-}
+  async function getCounter() {
+    try {
+      const response = await fetch(workerURL, {
+        method: 'POST',
+      });
+      setCounter(await consumeStream(response));
+    } catch (error) {
+      console.error('Error fetching counter:', error);
+    }
+  }
 
   useEffect(() => {
     getCounter();
   }, []);
 
-  
-async function putCounter() {
-  const response = await fetch(workerURL, {
-    method: 'PUT',
-  });
-  updateCounter(await consumeStream(response));
-}
+  async function putCounter() {
+    try {
+      // Disable the button to prevent multiple clicks
+      setButtonDisabled(true);
 
-async function consumeStream(response) {
-  return await response.text()
-}
-  
+      const response = await fetch(workerURL, {
+        method: 'PUT',
+      });
+      setCounter(await consumeStream(response));
+    } catch (error) {
+      console.error('Error updating counter:', error);
+    } finally {
+      // Re-enable the button after the request is complete
+      setButtonDisabled(false);
+    }
+  }
+
+  async function consumeStream(response) {
+    return await response.text();
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>
-          No.
-        </h1>
-        <p> People waiting: {counter} </p>
-        <button onClick={() => putCounter()}> Wait </button>
+        <h1>No.</h1>
+        <p>People waiting: {counter}</p>
+        <button onClick={() => putCounter()} disabled={buttonDisabled} className="wait-button">
+          Wait
+        </button>
       </header>
     </div>
   );
 }
-
-
 
 export default App;
