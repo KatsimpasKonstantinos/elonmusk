9d565020310edfb472515ec9f62abd95aae111d7
KatsimpasKonstantinos <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2023-12-08 11:57:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,9 +8,25 @@ function App() {
         <h1>
           No.
         </h1>
+        <p> People waiting: {getCounter()} </p>
       </header>
     </div>
   );
+}
+
+const workerURL = "https://elonmuskdeathwaiters.konsti032003.workers.dev/";
+
+function getCounter() {
+  const response = await fetch(workerURL, {
+    method: 'POST',
+  });
+  return response.data;
+}
+
+function updateCounter() {
+  const response = await fetch(workerURL, {
+    method: 'PUT',
+  });
 }
 
 export default App;
