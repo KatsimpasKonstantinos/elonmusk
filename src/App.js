8d33d4d4ff95c9f1d4e66ab8acb1b7acbc84d75b
Miku <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2024-01-25 04:10:48 +0100

    visual overhaul

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
+import NotDead from "./NotDead.png";
+import Dead from "./Dead.png";
+
 
 function App() {
   const [counter, setCounter] = useState(0);
   const [buttonDisabled, setButtonDisabled] = useState(false);
+  const isDead = false;
 
   const workerURL = "https://elonmuskdeathwaiters.konsti032003.workers.dev/";
 
@@ -43,9 +47,16 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <marquee><h1>No.</h1></marquee>
-        <p>People waiting: {counter}</p>
-        <button onClick={() => putCounter()} disabled={buttonDisabled} className="wait-button">
+        <div className="profile-container">
+          <img
+            src={isDead ? Dead : NotDead}
+            alt="Profile"
+            className={"profile-picture"}
+          />
+        </div>
+        <h1 className={"neon-text-glowing"}>{isDead ? "Yes." : "No."}</h1>
+        <p className={"neon-text"}>People waiting: {counter}</p>
+        <button onClick={() => putCounter()} disabled={buttonDisabled} className="neon-button">
           Wait
         </button>
       </header>
