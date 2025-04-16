abc24dbb3477ebcd9dedae4ad9d5f402c553d0ec
Miku <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2025-04-16 15:38:26 +0200

    counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 
 import cat from './assets/cat.webp'
@@ -12,8 +13,23 @@ import ogar from './assets/ogar.webp'
 import sink from './assets/sink.webp'
 import wario from './assets/wario.webp'
 import weed from './assets/weed.webp'
+import { useEffect } from 'react'
 
 function App() {
+  const [email, setEmail] = useState('')
+  const [counter, setCounter] = useState(0)
+  const [status, setStatus] = useState('')
+
+  console.log(counter);
+
+  const workerURL = "https://elonmuskdeathwaiters.konsti032003.workers.dev/"
+
+  useEffect(() => {
+    fetch(workerURL)
+      .then(res => res.json())
+      .then(data => setCount(data.count ?? '?'));
+  }, []);
+
   return (
     <div className="App">
       <div className='grid'>
@@ -29,6 +45,16 @@ function App() {
         <img src={sink} alt="sink" className='image' />
         <img src={wario} alt="wario" className='image' />
         <img src={weed} alt="weed" className='image' />
+      </div>
+      <div className='form'>
+        <h2>Is Elon Musk dead?</h2>
+        <h1>No</h1>
+        <p>People Waiting: <span id="counter">...</span></p>
+        <p>Be the first one to know when he is dead</p>
+        <form id="waitlist-form">
+          <input type="email" name="email" placeholder="Your email..." required />
+          <button type="submit">Join Waitlist</button>
+        </form>
       </div>
     </div>
   )
