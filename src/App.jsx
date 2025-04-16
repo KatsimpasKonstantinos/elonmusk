96d576854cdde7421126063e64f31339f696c9df
Miku <134274002+KatsimpasKonstantinos@users.noreply.github.com>
2025-04-16 14:56:50 +0200

    images

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import cat from './assets/cat.webp'
+import cheese from './assets/cheese.webp'
+import cum from './assets/cum.webp'
+import cumming from './assets/cumming.webp'
+import devil from './assets/devil.webp'
+import hacked from './assets/hacked.webp'
+import hitler from './assets/hitler.webp'
+import noise from './assets/noise.webp'
+import ogar from './assets/ogar.webp'
+import sink from './assets/sink.webp'
+import wario from './assets/wario.webp'
+import weed from './assets/weed.webp'
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <div className='grid'>
+        <img src={cat} alt="cat" className='image' />
+        <img src={cheese} alt="cheese" className='image' />
+        <img src={cum} alt="cum" className='image' />
+        <img src={cumming} alt="cumming" className='image' />
+        <img src={devil} alt="devil" className='image' />
+        <img src={hacked} alt="hacked" className='image' />
+        <img src={hitler} alt="hitler" className='image' />
+        <img src={noise} alt="noise" className='image' />
+        <img src={ogar} alt="ogar" className='image' />
+        <img src={sink} alt="sink" className='image' />
+        <img src={wario} alt="wario" className='image' />
+        <img src={weed} alt="weed" className='image' />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
